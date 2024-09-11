acf7a377fe45cc24c21668062cb8ef8b0ac71fca
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2024-09-11 14:28:58 +0200

    update with tree

--- invmass/invmass.py
+++ invmass/invmass.py
@@ -2,6 +2,7 @@ import ROOT
 import math
 import random
 import tqdm
+import sys
 
 def generate_and_decay(parent_mass, daughter_mass_1, daughter_mass_2):
   # Generate random values for momentum and direction
@@ -20,9 +21,20 @@ def generate_and_decay(parent_mass, daughter_mass_1, daughter_mass_2):
   # Decay
   daughter1, daughter2 = two_body_decay(parent, daughter_mass_1, daughter_mass_2)
 
+  if 0:
+    print(f'parent: {parent}, m = {parent.M()}')
+    print(f'daughter1: {daughter1}, m = {daughter1.M()}')
+    print(f'daughter2: {daughter2}, m = {daughter2.M()}')
+    #sys.exit(0)
+
   # Smear momentum of daughter tracks
   daughter1 = smear_momentum(daughter1)
   daughter2 = smear_momentum(daughter2)
+
+  if 0:
+    print(f'daughter1: {daughter1}, m = {daughter1.M()}')
+    print(f'daughter2: {daughter2}, m = {daughter2.M()}')
+    sys.exit(0)
 
   return [daughter1, daughter2]
 
@@ -39,14 +51,6 @@ def two_body_decay(parent, m1, m2):
   # Energy of the daughters in the rest frame
   # In the rest frame, the daughters have equal momenta (p12)
   p12 = math.sqrt((m0**2 - (m1 + m2)**2) * (m0**2 - (m1 - m2)**2)) / (2 * m0)
-  # E0 = E1 + E2
-  # E0 = m0
-  # E1 = sqrt(m1**2 + p12**2)
-  # E2 = sqrt(m2**2 + p12**2)
-  # m0 = sqrt(m1**2 + p12**2) + sqrt(m2**2 + p12**2)
-  # m0**2 = m1**2 + m2**2 + 2 * p12**2 + 2 * sqrt((m1**2 + p12**2) * (m2**2 + p12**2))
-
-  # p12**2 = (E1**2 + E2**2 - m1**2 - m2**2) / 2
   E1 = (m0**2 + m1**2 - m2**2) / (2 * m0)
   E2 = (m0**2 + m2**2 - m1**2) / (2 * m0)
 
@@ -69,66 +73,96 @@ def two_body_decay(parent, m1, m2):
 
   # Boost daughters back to the lab frame if the parent is moving
   boost_vector = ROOT.Math.Boost(parent.BoostToCM())
-  daughter1 = boost_vector(daughter1)
-  daughter2 = boost_vector(daughter2)
+  daughter1 = ROOT.Math.PxPyPzMVector(boost_vector(daughter1))
+  daughter2 = ROOT.Math.PxPyPzMVector(boost_vector(daughter2))
 
   return daughter1, daughter2
 
 
 
 if __name__ == '__main__':
-  # Set the number of particles to generate
-  nParticles = 1000
-  #nParticles = 10
+  if 1:
+    # Set the number of particles to generate
+    nParticles = 100000
+    #nParticles = 10
 
-  # Create a histogram to store generated momenta
-  #hMomentum = ROOT.TH1F("hMomentum", "Particle Momentum", 100, 0, 10)
-  hInvMass = ROOT.TH1F("hInvMass", "Invariant Mass", 300, 0, 3)
+    # Particle masses in GeV [https://pdg.lbl.gov/]
+    mass_pi_ch = 0.13957
+    mass_k_zero = 0.497611
+    mass_d_zero = 1.86484
 
-  # Particle masses
-  mass_pi_ch = 0.13957
-  mass_k_ch = 0.493677
-  mass_k_zero = 0.497611
-  mass_d_zero = 1.86484
-  #mass_dplus = 1.86966
-  mass_bzero = 5.27972
+    # random seed for reproducible results
+    random.seed(42)
 
-  # seed
-  random.seed(42)
+    # Create a histogram to store generated momenta
+    hInvMass = ROOT.TH1F("hInvMass", "Invariant Mass", 300, 0, 3)
 
-  # Loop over particles and generate random kinematics
-  #for i in range(nParticles):
-  for i in tqdm.tqdm(range(nParticles)):
-    tracks = []
-    tracks += generate_and_decay(mass_k_zero, mass_pi_ch, mass_pi_ch)
-    #tracks += generate_and_decay(mass_d_zero, mass_pi_ch, mass_k_ch)
-    tracks += generate_and_decay(mass_d_zero, mass_pi_ch, mass_pi_ch)
+    # Create tree (store events)
+    fileout = ROOT.TFile("tracks.root", "recreate")
+    tree = ROOT.TTree('tree', 'Tree with tracks')
+    tracks_vec = ROOT.std.vector(ROOT.Math.PxPyPzMVector)()
+    branch = tree.Branch('tracks', tracks_vec)
 
-    #assert len(tracks) == 2
-    for itr1 in range(len(tracks)):
-      for itr2 in range(itr1 + 1, len(tracks)):
-        hInvMass.Fill((tracks[itr1] + tracks[itr2]).M())
-  
+    # Loop over particles and generate random kinematics
+    #for i in range(nParticles):
+    for i in tqdm.tqdm(range(nParticles)):
+      tracks = []
+      tracks += generate_and_decay(mass_k_zero, mass_pi_ch, mass_pi_ch)
+      tracks += generate_and_decay(mass_d_zero, mass_pi_ch, mass_pi_ch)
+      assert len(tracks) == 4
+
+      tracks_vec.clear()
+      for tr in tracks:
+        tracks_vec.push_back(tr)
+      tree.Fill()
+
+      for itr1 in range(len(tracks)):
+        for itr2 in range(itr1 + 1, len(tracks)):
+          hInvMass.Fill((tracks[itr1] + tracks[itr2]).M())
+
+    # Write TTree to file
+    if 0:
+      fileout.cd()
+      tree.Write()
+      fileout.Close()
+
+  # Read data from TTree
+  if 0:
+    filein = ROOT.TFile("tracks.root")
+    tree = filein.Get('tree')
+    tracks = ROOT.std.vector(ROOT.Math.PxPyPzMVector)()
+    tree.SetBranchAddress('tracks', tracks)
+    hInvMass = ROOT.TH1F("hInvMass", "Invariant Mass", 300, 0, 3)
+    for ientr in range(tree.GetEntries()):
+      tree.GetEntry(ientr)
+      for itr1 in range(len(tracks)):
+        for itr2 in range(itr1 + 1, len(tracks)):
+          hInvMass.Fill((tracks[itr1] + tracks[itr2]).M())
+
   # Fit
-  fitFunc = ROOT.TF1("fitFunc", "[0]/(sqrt(2*TMath::Pi())*[2])*TMath::Exp(-0.5*((x-[1])/[2])^2)"   # First Gaussian
-                                      " + [3]/(sqrt(2*TMath::Pi())*[5])*TMath::Exp(-0.5*((x-[4])/[5])^2)" # Second Gaussian
-#                                      " + [6] + [7]*x + [8]*x^2 + [9]*x^3",               # Polynomial (2nd degree)
-                                      " + pol5(6)",               # Polynomial
+  fitFunc = ROOT.TF1("fitFunc", 
+                     "[0]/(sqrt(2*TMath::Pi())*[2])*TMath::Exp(-0.5*((x-[1])/[2])^2)"   # First Gaussian
+                     " + [3]/(sqrt(2*TMath::Pi())*[5])*TMath::Exp(-0.5*((x-[4])/[5])^2)" # Second Gaussian
+                     " + [6] + [7]*x + [8]*x^2 + [9]*x^3 + [10]*x^4 + [11]*x^5",               # Polynomial (5th degree)
+                     #"gaus(0) + gaus(3) + pol5(6)",               # Gaus + Gaus + Polynomial (5th degree)
                           -10, 10)
   fitFunc.SetParameters(1, 0.5, 0.01,  # Amplitude, mean, sigma of first Gaussian
                         1, 1.85, 0.05,  # Amplitude, mean, sigma of second Gaussian
-                        0, 0, 0, 0);    # Coefficients of the polynomial
-  fitFunc.SetParLimits(1, 0.48, 0.52)
-  fitFunc.SetParLimits(2, 0., 0.1)
-  fitFunc.SetParLimits(4, 1.8, 1.9)
-  fitFunc.SetParLimits(5, 0., 0.2)
+                        0, 0, 0, 0, 0);    # Coefficients of the polynomial (0 by default anyway)
+  #print(fitFunc.GetParameter(11))
+  #fitFunc.SetParameter(11, 10.0)
+  #print(fitFunc.GetParameter(11))
+  fitFunc.SetParLimits(1, 0.48, 0.52) # K0 mass
+  fitFunc.SetParLimits(2, 0., 0.1) # K0 width
+  fitFunc.SetParLimits(4, 1.8, 1.9) # D0 mass
+  fitFunc.SetParLimits(5, 0., 0.2) # D0 width
   hInvMass.Fit(fitFunc)
-  bin_width = (hInvMass.GetBinLowEdge(hInvMass.GetNbinsX() + 1) - hInvMass.GetBinLowEdge(1)) / hInvMass.GetNbinsX()
+  bin_width = hInvMass.GetBinLowEdge(2) - hInvMass.GetBinLowEdge(1)
   print(f'Number of signal events #1 = {fitFunc.GetParameter(0)/bin_width} +- {fitFunc.GetParError(0)/bin_width}')
   print(f'Number of signal events #2 = {fitFunc.GetParameter(3)/bin_width} +- {fitFunc.GetParError(3)/bin_width}')
 
   # Create a canvas to draw the histogram
-  canvas = ROOT.TCanvas("canvas", "Invariant Mass", 800, 600)
+  canvas = ROOT.TCanvas("canvas", "Invariant Mass", 600, 600)
   hInvMass.Draw()
 
   # Save the histogram as an image
